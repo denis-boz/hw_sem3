a08cae2113df2375fa8c6d7c0578e4542dca2e92
Denis <denis-boz@hotmail.com>
2023-06-04 22:37:50 +0300

    Решение задачи 21

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@
 23432 -> да
 */
 
+
+/*
 Console.Write("Введите пятизначное число: ");
 var numb = Console.ReadLine();
 int length = numb.Length;
@@ -24,3 +26,29 @@ else
 {
     Console.WriteLine("Вы ввели неверное число");
 }
+*/
+
+/*
+Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.Write("Введите координату для x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату для y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату для z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите координату для x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату для y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату для z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+
+Console.WriteLine($"Расстояние между точками A:({x1}, {y1}, {z1}) и B:({x2}, {y2}, {z2}) равно: {Math.Round(d, 2)}");
